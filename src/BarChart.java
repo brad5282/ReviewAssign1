a26a4ccb816898f379b87a7ef397ad3a6cd779a7
brad5282 <brad5282@ISS2TNQKH2.scdsb.pri>
2020-09-15 12:14:41 -0400

    modifications were made

--- src/BarChart.java
+++ src/BarChart.java
@@ -1,8 +1,11 @@
 import TurtleGraphics.*;
+import java.util.Scanner;
 public class BarChart {
 
-  
+    static SketchPadWindow win = new SketchPadWindow(850,670);
+    static Pen p = new StandardPen(win);
     public static void main(String[] args) {
+       Scanner s = new Scanner(System.in);
        
     }
     
