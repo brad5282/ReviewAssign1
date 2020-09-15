b31ca54143ac84b12c056039630b33dd444f9488
brad5282 <brad5282@ISS2TNQKH2.scdsb.pri>
2020-09-15 12:17:27 -0400

    modifications were made

--- src/BarChart.java
+++ src/BarChart.java
@@ -6,6 +6,16 @@ public class BarChart {
     static Pen p = new StandardPen(win);
     public static void main(String[] args) {
        Scanner s = new Scanner(System.in);
+       int v1, v2, v3, v4;
+       
+        System.out.println("Enter value 1 of 4: ");
+        v1 = s.nextInt();
+        System.out.println("Enter value 2 of 4: ");
+        v2 = s.nextInt();
+        System.out.println("Enter value 3 of 4: ");
+        v3 = s.nextInt();
+        System.out.println("Enter value 4 of 4: ");
+        v4 = s.nextInt();
        
     }
     
