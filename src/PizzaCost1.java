e25afef7502e26594b5a40aecdaf427a8bc69c78
brad5282 <brad5282@ISS2TNQKH2.scdsb.pri>
2020-09-15 12:09:26 -0400

    modifications were made

--- src/PizzaCost1.java
+++ src/PizzaCost1.java
@@ -7,9 +7,9 @@ public class PizzaCost1 {
    
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        double topping, total,labour, heat, ingredients, size ;
-        labour = 1.50;
-        heat = 0.75;
+        double topping, total,size ;
+        final double labour = 1.50;
+        final double heat = 0.75;
         
         System.out.print("Please enter the size of your pizza in inches:");
         size = s.nextDouble();
