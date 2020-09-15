127834a8eab10d62df67d34435a3abd935271c4f
brad5282 <brad5282@ISS2TNQKH2.scdsb.pri>
2020-09-15 12:34:38 -0400

    modifications were made

--- src/BarChart.java
+++ src/BarChart.java
@@ -1,22 +1,52 @@
 import TurtleGraphics.*;
+import java.awt.Color;
 import java.util.Scanner;
 public class BarChart {
 
-    static SketchPadWindow win = new SketchPadWindow(850,670);
+    static SketchPadWindow win = new SketchPadWindow(500,400);
     static Pen p = new StandardPen(win);
     public static void main(String[] args) {
        Scanner s = new Scanner(System.in);
        int v1, v2, v3, v4;
        
-        System.out.println("Enter value 1 of 4: ");
-        v1 = s.nextInt();
-        System.out.println("Enter value 2 of 4: ");
-        v2 = s.nextInt();
-        System.out.println("Enter value 3 of 4: ");
-        v3 = s.nextInt();
-        System.out.println("Enter value 4 of 4: ");
-        v4 = s.nextInt();
+       //drawing x axis
+        p.setColor(Color.black);
+        p.setWidth(10);
+        p.up();
+        p.move(-250,-150);
+        p.down();
+        p.move(250,-150);
        
+        System.out.print("Enter value 1 of 4: ");
+        v1 = s.nextInt();
+        System.out.print("Enter value 2 of 4: ");
+        v2 = s.nextInt();
+        System.out.print("Enter value 3 of 4: ");
+        v3 = s.nextInt();
+        System.out.print("Enter value 4 of 4: ");
+        v4 = s.nextInt();
+        
+        
+        
+        
+        //drawing bars
+        p.setWidth(50);
+        makeBar(p, 0,-120, v1, Color.red);
+       
+    }
+    
+    public static void makeBar(Pen p, int xloc, int yloc, int length, Color col){
+        p.up();
+        p.move(xloc, yloc);
+        p.down();
+        p.setColor(col);
+        p.setDirection(90);
+        p.move(length);
+        p.up();
+        p.move(15);
+        p.down();
+        p.drawString(length+"");
+    
     }
     
 }
