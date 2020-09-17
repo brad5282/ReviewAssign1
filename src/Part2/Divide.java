33b447ef9cab5d861c541d83482e1e9db13c77cf
brad5282 <brad5282@ISS2TNQKH2.scdsb.pri>
2020-09-17 10:22:22 -0400

    newer assignments

--- src/Part2/Divide.java
+++ src/Part2/Divide.java
@@ -1,21 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Part2;
 
-/**
- *
- * @author brad5282
- */
+import java.util.Scanner;
+
+
 public class Divide {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner s = new Scanner(System.in);
+        int num1, num2,rem, a, max, min;
+        
+        //gathering numbers
+        System.out.print("Enter a number: ");
+        num1 = s.nextInt();
+        System.out.print("Enter another number: ");
+        num2 = s.nextInt();
+        
+        //determining the max & min
+        max = Math.max(num1, num2);
+        min = Math.min(num1, num2);
+        System.out.println(max + " is the larger number, and the smaller number is " + min);
+        
+        //calculations
+        a = max/min;
+        rem = max % min;
+        
+        //print
+        System.out.format("The answer is %dr %d\n", a,rem);
     }
     
 }
--- src/Part2/PowerChart.java
+++ src/Part2/PowerChart.java
@@ -1,21 +1,200 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Part2;
 
-/**
- *
- * @author brad5282
- */
+import java.util.Scanner;
+
+
 public class PowerChart {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner s = new Scanner(System.in);
+        double exp, a,a1,a2,a3,a4,a5,a6,a7,a8,a9,a10;
+        
+        while(true){
+            System.out.print("Enter a number between 0-10: ");
+            exp = s.nextInt();
+            if(exp>=0 && exp <= 10) break;
+            System.out.print("Error, from 0-10. Try Again \n");
+        }
+        
+        if(exp == 0){
+            a = Math.pow(2,exp);
+            System.out.println(" X                   2^X\n");
+            System.out.println("===                 ======= \n ");
+            System.out.format("%.2f                  %.2f\n ", exp, a);   
+        }
+        else if(exp == 1){
+            a = Math.pow(2,0);
+            a1 = Math.pow(2, exp);
+            System.out.println(" X                   2^X\n");
+            System.out.println("===                 ======= \n ");
+            System.out.format("%.2f                  %.2f\n ", 0.0, a);
+            System.out.format("%.2f                  %.2f\n ", exp, a1);
+        }
+        else if(exp == 2){
+            a = Math.pow(2,0);
+            a1 = Math.pow(2, 1);
+            a2 = Math.pow(2, exp);
+            System.out.println(" X                   2^X\n");
+            System.out.println("===                 ======= \n ");
+            System.out.format("%.2f                  %.2f\n ", 0.0, a);
+            System.out.format("%.2f                  %.2f\n ", 1.0, a1);
+            System.out.format("%.2f                  %.2f\n ", exp, a2);
+        }
+        else if(exp == 3){
+            a = Math.pow(2,0);
+            a1 = Math.pow(2, 1);
+            a2 = Math.pow(2, 2);
+            a3 = Math.pow(2,exp);
+            System.out.println(" X                   2^X\n");
+            System.out.println("===                 ======= \n ");
+            System.out.format("%.2f                  %.2f\n ", 0.0, a);
+            System.out.format("%.2f                  %.2f\n ", 1.0, a1);
+            System.out.format("%.2f                  %.2f\n ", 2.0, a2);
+            System.out.format("%.2f                  %.2f\n ", exp, a3);
+        }
+        else if(exp == 4){
+            a = Math.pow(2,0);
+            a1 = Math.pow(2, 1);
+            a2 = Math.pow(2, 2);
+            a3 = Math.pow(2,3);
+            a4 = Math.pow(2,exp);
+            System.out.println(" X                   2^X\n");
+            System.out.println("===                 ======= \n ");
+            System.out.format("%.2f                  %.2f\n ", 0.0, a);
+            System.out.format("%.2f                  %.2f\n ", 1.0, a1);
+            System.out.format("%.2f                  %.2f\n ", 2.0, a2);
+            System.out.format("%.2f                  %.2f\n ", 3.0, a3);
+            System.out.format("%.2f                  %.2f\n ", exp, a4);
+        }
+        else if(exp == 5){
+            a = Math.pow(2,0);
+            a1 = Math.pow(2, 1);
+            a2 = Math.pow(2, 2);
+            a3 = Math.pow(2,3);
+            a4 = Math.pow(2,4);
+            a5 =Math.pow(2, exp);
+            System.out.println(" X                   2^X\n");
+            System.out.println("===                 ======= \n ");
+            System.out.format("%.2f                  %.2f\n ", 0.0, a);
+            System.out.format("%.2f                  %.2f\n ", 1.0, a1);
+            System.out.format("%.2f                  %.2f\n ", 2.0, a2);
+            System.out.format("%.2f                  %.2f\n ", 3.0, a3);
+            System.out.format("%.2f                  %.2f\n ", 4.0, a4);
+            System.out.format("%.2f                  %.2f\n ", exp, a5);
+        }
+        else if(exp == 6){
+            a = Math.pow(2,0);
+            a1 = Math.pow(2, 1);
+            a2 = Math.pow(2, 2);
+            a3 = Math.pow(2,3);
+            a4 = Math.pow(2,4);
+            a5 =Math.pow(2, 5);
+            a6 =Math.pow(2, exp);
+            System.out.println(" X                   2^X\n");
+            System.out.println("===                 ======= \n ");
+            System.out.format("%.2f                  %.2f\n ", 0.0, a);
+            System.out.format("%.2f                  %.2f\n ", 1.0, a1);
+            System.out.format("%.2f                  %.2f\n ", 2.0, a2);
+            System.out.format("%.2f                  %.2f\n ", 3.0, a3);
+            System.out.format("%.2f                  %.2f\n ", 4.0, a4);
+            System.out.format("%.2f                  %.2f\n ", 5.0, a5);
+            System.out.format("%.2f                  %.2f\n ", exp, a6);
+        }
+        else if(exp == 7){
+            a = Math.pow(2,0);
+            a1 = Math.pow(2, 1);
+            a2 = Math.pow(2, 2);
+            a3 = Math.pow(2,3);
+            a4 = Math.pow(2,4);
+            a5 =Math.pow(2, 5);
+            a6 =Math.pow(2, 6);
+            a7 =Math.pow(2, exp);
+            System.out.println(" X                   2^X\n");
+            System.out.println("===                 ======= \n ");
+            System.out.format("%.2f                  %.2f\n ", 0.0, a);
+            System.out.format("%.2f                  %.2f\n ", 1.0, a1);
+            System.out.format("%.2f                  %.2f\n ", 2.0, a2);
+            System.out.format("%.2f                  %.2f\n ", 3.0, a3);
+            System.out.format("%.2f                  %.2f\n ", 4.0, a4);
+            System.out.format("%.2f                  %.2f\n ", 5.0, a5);
+            System.out.format("%.2f                  %.2f\n ", 6.0, a6);
+            System.out.format("%.2f                  %.2f\n ", exp, a7);
+        }
+        else if(exp == 8){
+            a = Math.pow(2,0);
+            a1 = Math.pow(2, 1);
+            a2 = Math.pow(2, 2);
+            a3 = Math.pow(2,3);
+            a4 = Math.pow(2,4);
+            a5 =Math.pow(2, 5);
+            a6 =Math.pow(2, 6);
+            a7 =Math.pow(2, 7);
+            a8 =Math.pow(2, exp);
+            System.out.println(" X                   2^X\n");
+            System.out.println("===                 ======= \n ");
+            System.out.format("%.2f                  %.2f\n ", 0.0, a);
+            System.out.format("%.2f                  %.2f\n ", 1.0, a1);
+            System.out.format("%.2f                  %.2f\n ", 2.0, a2);
+            System.out.format("%.2f                  %.2f\n ", 3.0, a3);
+            System.out.format("%.2f                  %.2f\n ", 4.0, a4);
+            System.out.format("%.2f                  %.2f\n ", 5.0, a5);
+            System.out.format("%.2f                  %.2f\n ", 6.0, a6);
+            System.out.format("%.2f                  %.2f\n ", 7.0, a7);
+            System.out.format("%.2f                  %.2f\n ", exp, a8);
+        }
+        else if(exp == 9){
+            a = Math.pow(2,0);
+            a1 = Math.pow(2, 1);
+            a2 = Math.pow(2, 2);
+            a3 = Math.pow(2,3);
+            a4 = Math.pow(2,4);
+            a5 =Math.pow(2, 5);
+            a6 =Math.pow(2, 6);
+            a7 =Math.pow(2, 7);
+            a8 =Math.pow(2, 8);
+            a9 =Math.pow(2, exp);
+            System.out.println(" X                   2^X\n");
+            System.out.println("===                 ======= \n ");
+            System.out.format("%.2f                  %.2f\n ", 0.0, a);
+            System.out.format("%.2f                  %.2f\n ", 1.0, a1);
+            System.out.format("%.2f                  %.2f\n ", 2.0, a2);
+            System.out.format("%.2f                  %.2f\n ", 3.0, a3);
+            System.out.format("%.2f                  %.2f\n ", 4.0, a4);
+            System.out.format("%.2f                  %.2f\n ", 5.0, a5);
+            System.out.format("%.2f                  %.2f\n ", 6.0, a6);
+            System.out.format("%.2f                  %.2f\n ", 7.0, a7);
+            System.out.format("%.2f                  %.2f\n ", 8.0, a8);
+            System.out.format("%.2f                  %.2f\n ", exp, a9);
+        }
+        else if(exp == 10){
+            a = Math.pow(2,0);
+            a1 = Math.pow(2, 1);
+            a2 = Math.pow(2, 2);
+            a3 = Math.pow(2,3);
+            a4 = Math.pow(2,4);
+            a5 =Math.pow(2, 5);
+            a6 =Math.pow(2, 6);
+            a7 =Math.pow(2, 7);
+            a8 =Math.pow(2, 8);
+            a9 =Math.pow(2, 9);
+            a10 =Math.pow(2, exp);
+            System.out.println(" X                   2^X\n");
+            System.out.println("===                 ======= \n ");
+            System.out.format("%.2f                  %.2f\n ", 0.0, a);
+            System.out.format("%.2f                  %.2f\n ", 1.0, a1);
+            System.out.format("%.2f                  %.2f\n ", 2.0, a2);
+            System.out.format("%.2f                  %.2f\n ", 3.0, a3);
+            System.out.format("%.2f                  %.2f\n ", 4.0, a4);
+            System.out.format("%.2f                  %.2f\n ", 5.0, a5);
+            System.out.format("%.2f                  %.2f\n ", 6.0, a6);
+            System.out.format("%.2f                  %.2f\n ", 7.0, a7);
+            System.out.format("%.2f                  %.2f\n ", 8.0, a8);
+            System.out.format("%.2f                  %.2f\n ", 9.0, a9);
+            System.out.format("%.2f                  %.2f\n ", exp, a10);
+        }
+        
     }
     
 }
