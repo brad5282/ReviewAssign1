b73d89bc17a222318161be210a13d2b3d6983b30
brad5282 <brad5282@ISS2TNQKH2.scdsb.pri>
2020-09-17 11:22:29 -0400

    finished

--- src/Part2/EmployeePay.java
+++ src/Part2/EmployeePay.java
@@ -1,21 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Part2;
 
-/**
- *
- * @author brad5282
- */
+import java.util.Scanner;
+
+
 public class EmployeePay {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner s = new Scanner(System.in);
+        double totpay =0, totot = 0, grandtot =0;
+        double hours, ot, pay,tot;
+        
+        for (int i = 1; i <= 5; i++) {
+            while(true){
+                System.out.print("Enter Hours Worked for Employee " + i + ": ");
+                hours = s.nextDouble();
+                if(hours >= 1 && hours <= 70) break;
+                else {
+                    System.out.println("Invalid, try again.");
+                    }
+            }
+            if(hours > 40){
+                ot = (hours - 40)* 22.5;
+                pay = 40 * 15;
+                tot = ot + pay; 
+            }
+            else{
+                pay = hours * 15;
+                ot = 0;
+                tot = pay + ot;
+            }
+            grandtot+= tot;
+            totot+= ot;
+            totpay += pay;
+            
+            System.out.println("Regular Pay: " + pay);
+            System.out.println("Overtime Pay: " + ot);
+            System.out.println("Total Pay: " + tot);
+            System.out.println("------------------------------\n");
+            
+            
+        }
+        System.out.println("Total Regular Pay: " + totpay);
+        System.out.println("Total Overtime Pay: " + totot);
+        System.out.println("Grand Total Pay: " + grandtot);
     }
     
 }
